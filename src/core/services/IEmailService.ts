611ca861f97b14f74a581fbb06b26b9a2d2ab3b3
Gustavo Amarista <zipnegocios@gmail.com>
2026-07-04 19:04:09 -0400

    feat: add email notification method signatures for stage changes and schedule events

--- src/core/services/IEmailService.ts
+++ src/core/services/IEmailService.ts
@@ -1,9 +1,18 @@
 import { Quote } from '../entities/Quote'
 import { Lead } from '../entities/Lead'
+import { LeadEvent } from '../entities/LeadEvent'
 
 export interface IEmailService {
   sendQuoteConfirmation(quote: Quote): Promise<void>
   sendAdminNotification(quote: Quote): Promise<void>
   sendNewMessageNotificationToAdmin(lead: Lead, quote: Quote): Promise<void>
   sendNewMessageNotificationToClient(quote: Quote): Promise<void>
+  sendStageChangeNotificationToClient(quote: Quote, stageLabel: string): Promise<void>
+  sendStageChangeNotificationToAdmin(lead: Lead, quote: Quote, fromLabel: string, toLabel: string): Promise<void>
+  sendEventScheduledNotificationToClient(quote: Quote, event: LeadEvent): Promise<void>
+  sendEventScheduledNotificationToAdmin(lead: Lead, quote: Quote, event: LeadEvent): Promise<void>
+  sendEventUpdatedNotificationToClient(quote: Quote, event: LeadEvent): Promise<void>
+  sendEventUpdatedNotificationToAdmin(lead: Lead, quote: Quote, event: LeadEvent): Promise<void>
+  sendEventCancelledNotificationToClient(quote: Quote, event: LeadEvent): Promise<void>
+  sendEventCancelledNotificationToAdmin(lead: Lead, quote: Quote, event: LeadEvent): Promise<void>
 }
